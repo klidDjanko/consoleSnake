a49f72ad496464f113c09a7a3b362a2625453b07
klidDjanko <service.1.storage@gmail.com>
2021-02-21 16:40:38 +0300

    Рисование точек, начало проекта

---
+++ snake/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        //Поля класса для координат и отображаемого символа-точки
+        public int x;
+        public int y;
+        public char symbol;
+
+        //Метод вывода точки-символа на консоль
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ snake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Создадим и выведем пару точек
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 5;
+            p1.symbol = '*';
+
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 10;
+            p2.symbol = '+';
+
+            p2.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
